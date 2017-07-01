0608edb0b8976c1a590a8de57ff4d42337415ecb
christopherrehm <christopherrehm@squeaker>
2017-07-01 17:33:26 +0200

    first for this dir Signed-off-by:christopherrehm <christopherrehm@squeaker>

---
+++ NetBeansProjects/grendelNewLIb/src/message.java
@@ -0,0 +1,48 @@
+public class message{
+
+    private long idNumber ;
+    private long myOrigin ;
+    private long myDestination;
+    private int actionCode;
+    private long myData;  
+    
+    public void setID(long id){
+        idNumber = id;
+    }
+    
+    public long showID(){
+        return idNumber;
+    }
+    
+    public void setOrigin (long id){
+        myOrigin = id;
+    }
+    
+    public long showOrigin(){
+        return myOrigin;   
+    }
+    
+    public void setDestination(long id){
+        myDestination = id;    
+    }
+    
+    public long showDestination(long id){
+        return myDestination;
+    }
+    
+    public void setDataID(long id){
+        myData = id;
+    }
+    
+    public long showDataID(){
+        return myData;    
+    }
+    
+    public void setActionCode(int theActionCode){
+        actionCode = theActionCode;
+    }
+    
+    public int getActionCode(){
+        return actionCode;
+    }
+}
