8d573b0a65c3c4719e121ff4a83ef497abfdbf7f
christopherrehm <christopherrehm@squeaker>
2017-07-02 20:36:13 +0200

    new file, tracks threads callable anywhere Signed-off-by:christopherrehm <christopherrehm@squeaker>

---
+++ NetBeansProjects/grendelNewLIb/src/basicstuff/ThreadTracker.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicstuff;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author christopherrehm
+ */
+public class ThreadTracker extends Thread {
+    public void run(){
+        while (true){
+            try {
+                Thread.getAllStackTraces().keySet().forEach((t) -> System.out.println("----------"+ t.getName() + " Is Daemon " + t.isDaemon() + " Is Alive " + t.isAlive()));
+                Thread.sleep(10000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ObjectStatus.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }     
+    }   
+}
