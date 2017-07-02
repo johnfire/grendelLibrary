95f2e24c6545bdd701e8ae5c0a050deef49c1378
christopherrehm <christopherrehm@squeaker>
2017-07-02 20:35:47 +0200

    seperated status update from main thread,minor tweaks Signed-off-by:christopherrehm <christopherrehm@squeaker>

---
+++ NetBeansProjects/grendelNewLIb/src/basicstuff/ObjectStatus.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicstuff;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+/**
+ *
+ * @author christopherrehm
+ */
+public class ObjectStatus extends Thread {
+    String myName;
+    int pid;
+    
+    public void setMyName(String WhoAmI) {
+        myName = WhoAmI;
+    }
+    
+    public void run(){
+        while (true){
+            try {
+                System.out.println("-----we are in the + " + myName + " routine, process number "+ pid);
+                Thread.getAllStackTraces().keySet().forEach((t) -> System.out.println("----------"+ t.getName() + " Is Daemon " + t.isDaemon() + " Is Alive " + t.isAlive()));
+                Thread.sleep(5000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ObjectStatus.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }     
+    }
+}
