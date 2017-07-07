52b54c1fce6a08f340fa39fc2cc2524a9540dcac
christopherrehm <christopherrehm@squeaker>
2017-07-07 23:07:25 +0200

    code cleanup Signed-off-by:christopherrehm <christopherrehm@squeaker>

--- NetBeansProjects/grendelNewLIb/src/basicstuff/Message.java
+++ NetBeansProjects/grendelNewLIb/src/basicstuff/Message.java
@@ -9,7 +9,6 @@ import java.io.Serializable;
 public class Message implements Serializable{
     
     private static int theMessageCounter = 0;
-    public boolean iAmLocked = false;
     int myMessageNr;
     int myID;
     int myOrigin;
